8d0af1478f64b5cc8e440f732bb4c36422015afe
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-05-23 00:00:00 -0300

    Adicionada funcionalidade de adição de 'post'

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
+from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+
+from .forms import PostForm
 from .models import Post
 
 
@@ -11,3 +14,17 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
