90e47c5ada24caa6865b11b520525d7d870bb0eb
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-05-24 00:00:00 -0300

    Adicionada funcionalidade de edição de 'post'

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
